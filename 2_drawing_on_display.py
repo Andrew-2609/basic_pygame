1d940a765451396cb1155fb8ff202a1f5c1893ec
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-08-27 20:56:59 -0300

    implementing the change of colors logic at the display update

--- 2_drawing_on_display.py
+++ 2_drawing_on_display.py
@@ -6,8 +6,21 @@ pygame.init()
 # Create a display surface and set its caption
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
-pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Drawing Objects")
+
+# Define colors as RGB tuples
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+
+# Give a background color to the display
+display_surface.fill(BLUE)
 
 # The main game loop
 running = True
@@ -16,6 +29,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Update the display
+    pygame.display.update()
 
 # End the game
 pygame.quit()
