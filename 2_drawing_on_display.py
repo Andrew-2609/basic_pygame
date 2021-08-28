585cd360b6553b0cd12f8d349724fd4783c6c657
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-08-27 22:15:29 -0300

    drawing geometric figures on display_surface

--- 2_drawing_on_display.py
+++ 2_drawing_on_display.py
@@ -20,7 +20,19 @@ CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 
 # Give a background color to the display
-display_surface.fill(BLUE)
+display_surface.fill(BLACK)
+
+# Draw various shapes on our display
+# Line ( surface, color, starting point, ending point, thickness )
+pygame.draw.line(display_surface, WHITE, (0, 0), (100, 100), 5)
+pygame.draw.line(display_surface, WHITE, (100, 100), (599, 300), 5)
+
+# Circle ( surface, color, center, radius, thickness... 0 for fill )
+pygame.draw.circle(display_surface, RED, (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 50), 200, 0)
+pygame.draw.circle(display_surface, WHITE, (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 25), 167, 0)
+
+# Rectangles ( surface, color, (top-left x, top-left y, width, height) )
+pygame.draw.rect(display_surface, WHITE, (250, 450, 100, 350))
 
 # The main game loop
 running = True
