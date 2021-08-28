a4dd88b58c5873d9a8d71541f3826a53afed96c0
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-08-27 22:28:01 -0300

    adapting the code to display the Uchiha symbol

--- 2_drawing_on_display.py
+++ 2_drawing_on_display.py
@@ -18,20 +18,26 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
+UCHIHA = (255, 30, 29)
 
 # Give a background color to the display
-display_surface.fill(BLACK)
+display_surface.fill(WHITE)
 
 # Draw various shapes on our display
 # Line ( surface, color, starting point, ending point, thickness )
-pygame.draw.line(display_surface, WHITE, (0, 0), (100, 100), 5)
-pygame.draw.line(display_surface, WHITE, (100, 100), (599, 300), 5)
+# pygame.draw.line(display_surface, WHITE, (0, 0), (100, 100), 5)
+# pygame.draw.line(display_surface, WHITE, (100, 100), (599, 300), 5)
 
 # Circle ( surface, color, center, radius, thickness... 0 for fill )
-pygame.draw.circle(display_surface, RED, (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 50), 200, 0)
-pygame.draw.circle(display_surface, WHITE, (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 25), 167, 0)
+pygame.draw.circle(display_surface, BLACK, (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 50), 210, 10)
+pygame.draw.circle(display_surface, UCHIHA, (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 50), 200, 0)
 
 # Rectangles ( surface, color, (top-left x, top-left y, width, height) )
+pygame.draw.rect(display_surface, BLACK, (240, 450, 120, 360))
+
+pygame.draw.circle(display_surface, BLACK, (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 15), 157, 10)
+pygame.draw.circle(display_surface, WHITE, (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 15), 147, 0)
+
 pygame.draw.rect(display_surface, WHITE, (250, 450, 100, 350))
 
 # The main game loop
