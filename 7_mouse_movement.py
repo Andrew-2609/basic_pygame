8395c9dde8f6a3a9345f37ae9c3c1adeef744f29
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-08-29 22:39:29 -0300

    displaying the dino_image at 7_mouse_movement.py

--- 7_mouse_movement.py
+++ 7_mouse_movement.py
@@ -7,7 +7,14 @@ pygame.init()
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+display_surface.fill((255, 255, 255))
 pygame.display.set_caption("Mouse Movement")
+
+# Load images
+dino_image = pygame.image.load("left_dino.png")
+dino_image = pygame.transform.flip(dino_image, True, False)
+dino_rect = dino_image.get_rect()
+dino_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
 
 # The main game loop
 running = True
@@ -17,7 +24,10 @@ while running:
             running = False
 
     # Clear the display
-    display_surface.fill((0, 0, 0))
+    display_surface.fill((255, 255, 255))
+
+    # Blit (copy) the assets to the display surface
+    display_surface.blit(dino_image, dino_rect)
 
     # Update the display
     pygame.display.update()
