03b64e44fdafef75aa2e5fa82fd6715ebaa14b63
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-08-29 22:05:20 -0300

    logic for playing, manipulating the volume and stopping sound effects and bgm implemented on 5_adding_sounds.py

--- 5_adding_sounds.py
+++ 5_adding_sounds.py
@@ -9,6 +9,30 @@ WINDOW_HEIGHT = 300
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Adding Sounds")
 
+# Loading sound effects
+sound_1 = pygame.mixer.Sound("sound_1.wav")
+sound_2 = pygame.mixer.Sound("sound_2.wav")
+
+# PLay the sound effects
+sound_1.play()
+pygame.time.delay(int(sound_1.get_length()) * 1000)
+sound_2.play()
+pygame.time.delay(2000)
+
+# Change the volume of the sound effect
+sound_2.set_volume(.1)
+sound_2.play()
+
+# Load background music
+pygame.mixer.music.load("music.wav")
+
+# Play and stop
+pygame.mixer.music.play(-1, 0.0)
+pygame.time.delay(2000)
+sound_1.play()
+pygame.time.delay(5000)
+pygame.mixer.music.stop()
+
 # The main game loop
 running = True
 while running:
