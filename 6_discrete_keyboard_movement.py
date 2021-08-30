b80d35b70c40228df17a7c238cee51908584c145
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-08-29 22:17:13 -0300

    basic implementation on 6_discrete_keyboard_movement.py for loading and inverting horizontally the left_dino.png image, and showing it on the display_surface, which is now white

--- 6_discrete_keyboard_movement.py
+++ 6_discrete_keyboard_movement.py
@@ -7,7 +7,18 @@ pygame.init()
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+display_surface.fill((255, 255, 255))
 pygame.display.set_caption("Discrete Keyboard Movement")
+
+# Set game values
+VELOCITY = 10
+
+# Load in images
+dino_image = pygame.image.load("left_dino.png")
+dino_image = pygame.transform.flip(dino_image, True, False)
+dino_rect = dino_image.get_rect()
+dino_rect.centerx = WINDOW_WIDTH // 2
+dino_rect.bottom = WINDOW_HEIGHT
 
 # The main game loop
 running = True
@@ -15,6 +26,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Blit (copy) assets to the screen
+    display_surface.blit(dino_image, dino_rect)
 
     # Update the display
     pygame.display.update()
