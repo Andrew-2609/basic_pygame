b3d7755e234b9ca751bdf1eb38ee22efa1454332
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-08-31 20:53:57 -0300

    game values set and images loaded and displayed, with display_surface filled

--- 8_continuous_keyboard_movement.py
+++ 8_continuous_keyboard_movement.py
@@ -9,12 +9,27 @@ WINDOW_HEIGHT = 400
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Continuous Keyboard Movement")
 
+# Set game values
+VELOCITY = 5
+
+# Load images
+dino_image = pygame.image.load("left_dino.png")
+dino_image = pygame.transform.flip(dino_image, True, False)
+dino_rect = dino_image.get_rect()
+dino_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+
 # The main game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Fill the display
+    display_surface.fill((255, 255, 255))
+
+    # Blit (copy) the assets into the display surface
+    display_surface.blit(dino_image, dino_rect)
 
     # Update the display
     pygame.display.update()
