f128d3fd30d11ca44cddac7b374417977673c534
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-08-31 21:56:51 -0300

    setting fps and clock, loading images and displaying them and setting a velocity in 10_collision_detection.py

--- 10_collision_detection.py
+++ 10_collision_detection.py
@@ -9,6 +9,23 @@ WINDOW_HEIGHT = 400
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Collision Detection")
 
+# Set FPS and Clock
+FPS = 60
+clock = pygame.time.Clock()
+
+# Set game values
+VELOCITY = 5
+
+# Load images
+dino_image = pygame.image.load("left_dino.png")
+dino_image = pygame.transform.flip(dino_image, True, False)
+dino_rect = dino_image.get_rect()
+dino_rect.topleft = (25, 25)
+
+coin_image = pygame.image.load("coin.png")
+coin_rect = coin_image.get_rect()
+coin_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+
 # The main game loop
 running = True
 while running:
@@ -16,8 +33,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # Fill the display surface
+    display_surface.fill((255, 255, 255))
+
+    # Blit (copy) the assets into display
+    display_surface.blit(dino_image, dino_rect)
+    display_surface.blit(coin_image, coin_rect)
+
     # Update the display
     pygame.display.update()
+
+    # Tick the clock
+    clock.tick(FPS)
 
 # End the game
 pygame.quit()
